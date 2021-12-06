9cc53d4aadc2671e5345762ad03aa40d2beb7ea9
Davi_Tolen <davitolen@outlook.com>
2021-12-06 11:12:35 -0300

    Completando página create-point

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -25,6 +25,9 @@ function getCities(){
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citySelect.innerHTML = ""
+    citySelect.disabled = true
+
     fetch(url)
     .then( res => res.json())
     .then( cities => {
@@ -46,3 +49,47 @@ function getCities(){
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect){
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+let selectedItems = []
+
+function handleSelectedItem(event){
+    console.log(event.target)
+    const itemLi = event.target
+
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    // verificar se existem itens selecionados
+    // se sim, pegá-los
+    const alreadySelected = selectedItems.findIndex( item => {
+        const itemFound = item == itemId // boolean
+        return itemFound
+    })
+
+    //se já estiver selecionado,
+    if(alreadySelected >= 0){
+        //tirar da seleção
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item != itemId //false
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems
+    }
+    else {
+        // se não estiver selecionado
+        // adicionar a seleção
+        selectedItems.push(itemId)
+    }
+    //atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems  
+}
